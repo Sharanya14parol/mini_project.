7aff7cff2088e06c72fc1232541c223b21214c87
Sharanya14parol <156458949+Sharanya14parol@users.noreply.github.com>
2024-12-05 21:44:38 +0530

    Replace your db password

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',   // replace with your MySQL username
-    password: 'shashwath_09',  // replace with your MySQL password
+    password: '******',  // replace with your MySQL password
     database: 'exam_db'      // replace with your database name
 });
 
